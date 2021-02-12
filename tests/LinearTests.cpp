8d71e0f1a53c0a0ac79df375ee6de901d3ac26c2
SergArtuh <artuh92@gmail.com>
2021-02-12 12:52:38 +0200

    Array member access tests

--- tests/LinearTests.cpp
+++ tests/LinearTests.cpp
@@ -323,3 +323,101 @@ TEST_CASE("Array initialization", "[Array]") {
 	}
 
 }
+
+
+TEST_CASE("Array accessor", "[Array]") {
+	float data0[] = { 1.f ,2.f ,3.f ,4.f ,5.f ,6.f ,7.f ,8.f };
+
+	const Size vecLen = 2;
+	const Size arrLen = sizeof(data0) / (sizeof(data0[0]) * vecLen);
+	const Size matLen = arrLen / vecLen;
+
+	{
+		lnr::Vec2fArray arr0(arrLen);
+		lnr::Vec2fArray arr1(arrLen);
+		lnr::Vec2fArray arr2(arrLen);
+
+		REQUIRE(arr0.IsValide());
+		REQUIRE(arr1.IsValide());
+		REQUIRE(arr2.IsValide());
+
+		for (int i = 0; i < arrLen; ++i) {
+			arr0[i][0] = data0[i * vecLen + 0];
+			arr0[i][1] = data0[i * vecLen + 1];
+
+			lnr::Vec2f & v0 = arr1[i];
+			v0[0] = data0[i * vecLen + 0];
+			v0[1] = data0[i * vecLen + 1];
+
+			arr2[i][0] = data0[i * vecLen + 0];
+			arr2[i][1] = data0[i * vecLen + 1];
+
+			lnr::Vec2f v1 = arr2[i];
+			v1[0] = -1.f;
+			v1[1] = -1.f;
+		}
+
+		for (int i = 0; i < arrLen; ++i) {
+			REQUIRE(arr0[i][0] == data0[i * vecLen + 0]);
+			REQUIRE(arr0[i][1] == data0[i * vecLen + 1]);
+
+			lnr::Vec2f & v = arr1[i];
+			REQUIRE(v[0] == data0[i * vecLen + 0]);
+			REQUIRE(v[1] == data0[i * vecLen + 1]);
+
+			REQUIRE(arr2[i][0] == data0[i * vecLen + 0]);
+			REQUIRE(arr2[i][1] == data0[i * vecLen + 1]);
+		}
+	}
+
+	{
+		lnr::Mat2fArray mat0(matLen);
+		lnr::Mat2fArray mat1(matLen);
+		lnr::Mat2fArray mat2(matLen);
+		lnr::Mat2fArray mat4(matLen);
+
+		for (int i = 0; i < matLen; ++i) {
+
+			lnr::Mat2f & m = mat1[i];
+			lnr::Mat2f md = mat4[i];
+
+			for (int j = 0; j < matLen; ++j) {
+
+				lnr::Vec2f & v = mat1[i][j];
+				for (int k = 0; k < matLen; ++k) {
+					const float val = data0[(i * matLen + j) * matLen + k];
+
+					mat0[i][j][k] = val;
+					m[j][k] = val;
+					v[k] = val;
+
+					mat4[i][j][k] = val;
+					md[j][k] = -1.f;
+
+				}
+			}	
+		}
+
+
+		for (int i = 0; i < matLen; ++i) {
+
+			lnr::Mat2f& m = mat1[i];
+			lnr::Mat2f md = mat4[i];
+
+			for (int j = 0; j < matLen; ++j) {
+
+				lnr::Vec2f& v = mat1[i][j];
+				for (int k = 0; k < matLen; ++k) {
+					const float val = data0[(i * matLen + j) * matLen + k];
+
+					REQUIRE(mat0[i][j][k] == val);
+					REQUIRE(m[j][k] == val);
+					REQUIRE(v[k] == val);
+
+					REQUIRE(mat4[i][j][k] == val);
+
+				}
+			}
+		}
+	}
+}
